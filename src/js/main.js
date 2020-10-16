fadb3acf2c2782a71b066069b7edb60dc84e1ddd
Ervin <hamoydoerik@gmail.com>
2020-10-16 14:39:05 +0300

    Added functionality for working with classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 import './lib/lib';
 
-$('.active').toggleDisplay();
+$('.active').addClass('hello', 'world');
+$('.active').removeClass('hello', 'world');
+$('.active').toggleClass('active').toggleClass('active');
+$('.active').addClass('hello', 'world');
