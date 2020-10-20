ff9f5f5fb7af08204b3a26bf781cbdc1b496b875
Ervin <hamoydoerik@gmail.com>
2020-10-20 18:38:03 +0300

    Created dynamic modals

--- dist/script.js
+++ dist/script.js
@@ -1901,35 +1901,99 @@ Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.dropdown-toggle').dropdo
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
+/* harmony import */ var core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/web.dom-collections.iterator */ "./node_modules/core-js/modules/web.dom-collections.iterator.js");
+/* harmony import */ var core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../core */ "./src/js/lib/core.js");
 
 
-_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.modal = function () {
+
+_core__WEBPACK_IMPORTED_MODULE_1__["default"].prototype.modal = function (created) {
   for (let i = 0; i < this.length; i++) {
     const target = this[i].getAttribute('data-target');
-    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(this[i]).click(e => {
+    Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(this[i]).click(e => {
       e.preventDefault();
-      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(target).fadeIn(500);
+      Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(target).fadeIn(500);
       document.body.style.overflow = 'hidden';
     });
-  }
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
+    closeElements.forEach(elem => {
+      Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(elem).click(() => {
+        Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(target).fadeOut(500);
+        document.body.style.overflow = '';
 
-  const closeElements = document.querySelectorAll('[data-close]');
-  closeElements.forEach(elem => {
-    Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])(elem).click(() => {
-      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
-      document.body.style.overflow = '';
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      });
     });
-  });
-  Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').click(e => {
-    if (e.target.classList.contains('modal')) {
-      Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('.modal').fadeOut(500);
-      document.body.style.overflow = '';
-    }
-  });
+    Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(target).click(e => {
+      if (e.target.classList.contains('modal')) {
+        Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(target).fadeOut(500);
+        document.body.style.overflow = '';
+
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      }
+    });
+  }
 };
 
-Object(_core__WEBPACK_IMPORTED_MODULE_0__["default"])('[data-toggle="modal"]').modal();
+Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])('[data-toggle="modal"]').modal();
+
+_core__WEBPACK_IMPORTED_MODULE_1__["default"].prototype.createModal = function ({
+  text,
+  btns
+} = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.setAttribute('id', this[i].getAttribute('data-target').slice(1)); // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+
+    const buttons = [];
+
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement('button');
+      btn.classList.add('btn', ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+
+      if (btns.settings[j][2]) {
+        btn.setAttribute('data-close', 'true');
+      }
+
+      if (btns.settings[j][3] && typeof btns.settings[j][3] === 'function') {
+        btn.addEventListener('click', btns.settings[j][3]);
+      }
+
+      buttons.push(btn);
+    }
+
+    modal.innerHTML = `
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <button class="close" data-close>
+                        <span>&times;</span>
+                    </button>
+                    <div class="modal-header">
+                        <div class="modal-title">
+                            ${text.title}
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        ${text.body}
+                    </div>
+                    <div class="modal-footer">
+                        
+                    </div>
+                </div>
+            </div>
+        `;
+    modal.querySelector(".modal-footer").append(...buttons);
+    document.body.appendChild(modal);
+    Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(this[i]).modal(true);
+    Object(_core__WEBPACK_IMPORTED_MODULE_1__["default"])(this[i].getAttribute('data-target')).fadeIn(500);
+  }
+};
 
 /***/ }),
 
@@ -2452,27 +2516,45 @@ __webpack_require__.r(__webpack_exports__);
 // console.log($('.some').closest('.findme'));
 // console.log($('.findme').siblings());
 // $('.findme').fadeIn(1000);
+// $('#first').click(() => {
+//     $('div').eq(1).fadeToggle(800);
+// });
+// $('[data-count="second"]').click(() => {
+//     $('div').eq(2).fadeToggle(800);
+// });
+// $('button').eq(2).click(() => {
+//     $('.w-500').fadeToggle(800);
+// });
+// $('.wrap').html(
+//     `
+//         <div class="dropdown">
+//             <button class="btn btn-primary dropdown-toggle" id="dropdownMenuBtn">Dropdown button</button>
+//             <div class="dropdown-menu" data-toggle-id="dropdownMenuBtn">
+//                 <a href="#" class="dropdown-item">Action #1</a>
+//                 <a href="#" class="dropdown-item">Action #2</a>
+//                 <a href="#" class="dropdown-item">Action #3</a>   
+//             </div>
+//         </div>
+//     `
+// );
+// $('.dropdown-toggle').dropdown();
 
-$('#first').click(() => {
-  $('div').eq(1).fadeToggle(800);
+$('#trigger').click(() => {
+  $('#trigger').createModal({
+    text: {
+      title: "Ervin's modal title",
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloremque dicta libero, a eos, ullam dolor impedit exercitationem pariatur esse beatae quo commodi nemo laborum dolores perferendis itaque sunt natus!'
+    },
+    btns: {
+      count: 3,
+      settings: [['Close', ['btn-danger', 'mr-10'], true], ['Save changes', ['btn-success', 'mr-10'], false, () => {
+        alert("Done!");
+      }], ['Another btn', ['btn-warning'], false, () => {
+        alert("Done2!");
+      }]]
+    }
+  });
 });
-$('[data-count="second"]').click(() => {
-  $('div').eq(2).fadeToggle(800);
-});
-$('button').eq(2).click(() => {
-  $('.w-500').fadeToggle(800);
-});
-$('.wrap').html(`
-        <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuBtn">Dropdown button</button>
-            <div class="dropdown-menu" data-toggle-id="dropdownMenuBtn">
-                <a href="#" class="dropdown-item">Action #1</a>
-                <a href="#" class="dropdown-item">Action #2</a>
-                <a href="#" class="dropdown-item">Action #3</a>   
-            </div>
-        </div>
-    `);
-$('.dropdown-toggle').dropdown();
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -13,28 +13,63 @@ import './lib/lib';
 // console.log($('.findme').siblings());
 // $('.findme').fadeIn(1000);
 
-$('#first').click(() => {
-    $('div').eq(1).fadeToggle(800);
-});
+// $('#first').click(() => {
+//     $('div').eq(1).fadeToggle(800);
+// });
 
-$('[data-count="second"]').click(() => {
-    $('div').eq(2).fadeToggle(800);
-});
+// $('[data-count="second"]').click(() => {
+//     $('div').eq(2).fadeToggle(800);
+// });
 
-$('button').eq(2).click(() => {
-    $('.w-500').fadeToggle(800);
-});
+// $('button').eq(2).click(() => {
+//     $('.w-500').fadeToggle(800);
+// });
 
-$('.wrap').html(
-    `
-        <div class="dropdown">
-            <button class="btn btn-primary dropdown-toggle" id="dropdownMenuBtn">Dropdown button</button>
-            <div class="dropdown-menu" data-toggle-id="dropdownMenuBtn">
-                <a href="#" class="dropdown-item">Action #1</a>
-                <a href="#" class="dropdown-item">Action #2</a>
-                <a href="#" class="dropdown-item">Action #3</a>   
-            </div>
-        </div>
-    `
-);
-$('.dropdown-toggle').dropdown();
+// $('.wrap').html(
+//     `
+//         <div class="dropdown">
+//             <button class="btn btn-primary dropdown-toggle" id="dropdownMenuBtn">Dropdown button</button>
+//             <div class="dropdown-menu" data-toggle-id="dropdownMenuBtn">
+//                 <a href="#" class="dropdown-item">Action #1</a>
+//                 <a href="#" class="dropdown-item">Action #2</a>
+//                 <a href="#" class="dropdown-item">Action #3</a>   
+//             </div>
+//         </div>
+//     `
+// );
+// $('.dropdown-toggle').dropdown();
+
+$('#trigger').click(() => {
+    $('#trigger').createModal({
+        text: {
+            title: "Ervin's modal title",
+            body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloremque dicta libero, a eos, ullam dolor impedit exercitationem pariatur esse beatae quo commodi nemo laborum dolores perferendis itaque sunt natus!'
+        },
+        btns: {
+            count: 3,
+            settings: [
+                [
+                    'Close',
+                    ['btn-danger', 'mr-10'],
+                    true
+                ],
+                [
+                    'Save changes',
+                    ['btn-success', 'mr-10'],
+                    false,
+                    () => {
+                        alert("Done!");
+                    }
+                ],
+                [
+                    'Another btn',
+                    ['btn-warning'],
+                    false,
+                    () => {
+                        alert("Done2!");
+                    }
+                ]
+            ]
+        }
+    });
+});
